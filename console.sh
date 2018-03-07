6db6ab1a82eda0f9951e658b149ba252f6f28065
wangxi <wangxi@qiyuanfin.com>
2018-03-07 10:47:05 +0800

    update console shell

--- console.sh
+++ console.sh
@@ -1,1 +1,40 @@
-geth --identity "private_net" --rpc --rpcport "8545" --rpccorsdomain "*" --datadir "/Users/xib/Desktop/pn/data" --port "30303" --ipcpath '/Users/xib/Library/Ethereum/geth.ipc' --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --networkid 82403 --nat "any"  --etherbase '0x61886e9da42ba58f333c31716c55f5437a8abde3' console
+#!/usr/bin/env bash
+
+
+root=`pwd`
+
+etherbase=$1
+
+if [ ${etherbase} ];then
+
+    if [ ${etherbase} == "wx" ];then
+        etherbase="0x26c9bf0665fa7ecf75c74aded192e1c466bedfd7"
+    fi
+
+    geth --identity "private_net" \
+     --rpc --rpcport "8545" \
+     --rpccorsdomain "*" \
+     --datadir "${root}/data" \
+     --port "30303" \
+     --ipcpath "${root}/data/geth.ipc" \
+     --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
+     --networkid 82403 \
+     --nat "any"  \
+     --etherbase "${etherbase}" \
+     console
+else
+    geth --identity "private_net" \
+     --rpc --rpcport "8545" \
+     --rpccorsdomain "*" \
+     --datadir "${root}/data" \
+     --port "30303" \
+     --ipcpath "${root}/data/geth.ipc" \
+     --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
+     --networkid 82403 \
+     --nat "any"  \
+     console
+fi
+
+
+
+
