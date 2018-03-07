cd53fe034e7afdfc678ac1a4d55591330cf04132
wangxi <wangxi@qiyuanfin.com>
2018-03-07 12:00:47 +0800

    update eth_pn

--- console.sh
+++ console.sh
@@ -7,50 +7,48 @@ osEnv=$1
 if [ ${osEnv} ];then
     if [ ${osEnv} == "1" ];then
         ipc="${HOME}/.ethereum/geth.ipc"
-        echo "${ipc}"
-    fi
-
-    if [ ${osEnv} == "2" ];then
+    elif [ ${osEnv} == "2" ];then
         ipc="${HOME}/Library/Ethereum/geth.ipc"
-        echo "${ipc}"
+    else
+        echo "linux 1 ; oxs = 2"
+        exit 1
     fi
 else
     echo "plz tall me your os env, linux = 1 or oxs = 2"
     exit 1
 fi
 
-#
-#etherbase=$2
-#
-#if [ ${etherbase} ];then
-#
-#    if [ ${etherbase} == "wx" ];then
-#        etherbase="0x26c9bf0665fa7ecf75c74aded192e1c466bedfd7"
-#    fi
-#
-#    geth --identity "private_net" \
-#     --rpc --rpcport "8545" \
-#     --rpccorsdomain "*" \
-#     --datadir "${root}/data" \
-#     --port "30303" \
-#     --ipcpath "/home/qydev/.ethereum/geth.ipc" \
-#     --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
-#     --networkid 82403 \
-#     --nat "any"  \
-#     --etherbase "${etherbase}" \
-#     console
-#else
-#    geth --identity "private_net" \
-#     --rpc --rpcport "8545" \
-#     --rpccorsdomain "*" \
-#     --datadir "${root}/data" \
-#     --port "30303" \
-#     --ipcpath "/home/qydev/.ethereum/geth.ipc" \
-#     --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
-#     --networkid 82403 \
-#     --nat "any"  \
-#     console
-#fi
+etherbase=$2
+
+if [ ${etherbase} ];then
+
+    if [ ${etherbase} == "wx" ];then
+        etherbase="0x26c9bf0665fa7ecf75c74aded192e1c466bedfd7"
+    fi
+
+    geth --identity "private_net" \
+     --rpc --rpcport "8545" \
+     --rpccorsdomain "*" \
+     --datadir "${root}/data" \
+     --port "30303" \
+     --ipcpath "${ipc}" \
+     --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
+     --networkid 82403 \
+     --nat "any"  \
+     --etherbase "${etherbase}" \
+     console
+else
+    geth --identity "private_net" \
+     --rpc --rpcport "8545" \
+     --rpccorsdomain "*" \
+     --datadir "${root}/data" \
+     --port "30303" \
+     --ipcpath "${ipc}" \
+     --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
+     --networkid 82403 \
+     --nat "any"  \
+     console
+fi
 
 
 
